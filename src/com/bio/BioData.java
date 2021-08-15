670a9b62de8ad4a36fce78a87c08fb5ce30f30be
Suren2708 <surendran2708@gmail.com>
2021-08-15 10:19:02 +0530

    Check

---
+++ src/com/bio/BioData.java
@@ -0,0 +1,49 @@
+package com.bio;
+
+public class BioData {
+
+	public static void main(String[] args) {
+		// byte,short,int,long,float,double,char,boolean,string
+
+		System.out.println("My Biography:");
+		String a = "Surendran G";
+		System.out.println("My Name is " + a);
+		byte b = 27;
+		System.out.println("My age is " + b);
+		short c = 2015;
+		System.out.println("My UG completion Year " + c);
+		int d = 9087671;
+		System.out.println("My account number is " + d);
+		long e = 8754141252l;
+		System.out.println("My Phone Number is " + e);
+		float f = 7.78f;
+		System.out.println("My CGPA is " + f);
+		double g = 45123.76;
+		System.out.println("My salary per month is " + g);
+		char h = 'M';
+		System.out.println("My Gender is " + h);
+		float p = 25.545f;
+		System.out.println("My BMI is " + p);
+		double q = 78.789;
+		System.out.println("My Weight is " + q);
+		int s = 160;
+		System.out.println("My height in cm:" + s);
+		String j = "Cognizant";
+		System.out.println("My working place is " + j);
+		String k = "Sathya";
+		System.out.println("My Spouce Name is " + k);
+		byte l = 27;
+		System.out.println("My Spouce age is " + l);
+		short m = 2014;
+		System.out.println("My spouce UG completion Year " + m);
+		int n = 908768771;
+		System.out.println("My Spouce account number is " + n);
+		long o = 8076763252l;
+		System.out.println("My Spouce Phone Number is " + o);
+		String t = "Cognizant";
+		System.out.println("My working place is " + j);
+		boolean i = true;
+		System.out.println("My Details are verified and " + i);
+
+	}
+}
